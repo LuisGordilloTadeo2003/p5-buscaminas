f944119916f907e0e85f7aeddbdf8bbf34029696
LuisGordilloTadeo2003 <luisgordi@outlook.com>
2024-10-09 21:01:28 +0100

    trabajo dia 9 de octubre

--- grid.js
+++ grid.js
@@ -1,68 +1,115 @@
-class Grid{
-    constructor(ancho = 40, alto = 20, cellsize = 20){
+class Grid {
+    constructor(ancho = 40, alto = 20, cellsize = 20) {
 
         this.ancho = ancho;
         this.alto = alto;
         this.cellSize = cellsize;
 
         this.celdas = new Array(ancho);
-        for(let i = 0; i <this.ancho; i++){
+        for (let i = 0; i < this.ancho; i++) {
             this.celdas[i] = new Array(alto);
         }
         this.reset();
-        this.colocaBombas(10);
-     
-    }
-    colocaBombas(cantidad = 10){
-        let contador = 0;
-       do {
-            let bombax = floor(random(this.ancho))
-            let bombay = floor(random(this.alto))
-            if(!this.celdas[bombax][bombay].bomba){
-                this.celdas[bombax][bombay].bomba = true;
-                contador++
-            }
-        
-       } while (contador < cantidad);
+        this.colocaBombas(100);
+        this.calculaVecinos();
     }
 
-    reset(){
-        for(let i = 0; i < this.ancho; i++){
+    isSafe(x, y) {
+        return (x >= 0 && x < this.ancho && y >= 0 && y < this.alto)
+    }
+
+    calculaVecinos() {
+        for (let i = 0; i < this.ancho; i++) {
             for (let j = 0; j < this.alto; j++) {
-                this.celdas[i][j] = {
-                    bomba:false,
-                    bandera:false,
-                    descubierta:false,
-                    vecinos:0,
+                if (this.celdas[i][j].bomba) {
+                    for (let x = i - 1; x <= i + 1; x++) {
+                        for (let y = j - 1; y <= j + 1; y++) {
+                            if (this.isSafe(x, y)) {
+                                this.celdas[x][y].vecinos++;
+                            }
+                        }
+                    }
                 }
             }
         }
     }
 
-    display(){
-        let mx = floor((mouseX)/this.cellSize)
-        let my = floor(mouseY/this.cellSize)
-       
-       
-        for(let x = 0; x < this.ancho; x++){
-          for(let y = 0; y < this.alto; y++){
-           
-            if(x == mx & y == my){
-      
-              fill("orange")
-            }else if(this.celdas[x][y].bomba){
-                fill("red")
-            }else{
-      
-              fill("lightblue")
+    colocaBombas(cantidad) {
+        let contador = 0;
+        do {
+            let bombax = floor(random(this.ancho))
+            let bombay = floor(random(this.alto))
+            if (!this.celdas[bombax][bombay].bomba) {
+                this.celdas[bombax][bombay].bomba = true;
+                contador++
             }
-            rect(x * this.cellSize, y * this.cellSize, this.cellSize, this.cellSize)
-            if(this.celdas[x][y].bomba){
-                text('💣', x * this.cellSize + 4, y * this.cellSize + 12)
+        } while (contador < cantidad);
+    }
+
+    reset() {
+        for (let i = 0; i < this.ancho; i++) {
+            for (let j = 0; j < this.alto; j++) {
+                this.celdas[i][j] = {
+                    bomba: false,
+                    bandera: false,
+                    descubierta: false,
+                    vecinos: 0,
+                }
             }
-          }
         }
     }
-    
-   
+
+    display() {
+        let mx = floor(mouseX / this.cellSize)
+        let my = floor(mouseY / this.cellSize)
+
+        for (let x = 0; x < this.ancho; x++) {
+            for (let y = 0; y < this.alto; y++) {
+                if (x == mx & y == my) {
+                    fill("black")
+                } else {
+                    fill("lightgrey")
+                }
+                rect(x * this.cellSize, y * this.cellSize, this.cellSize, this.cellSize)
+
+                if (this.celdas[x][y].descubierta) {
+                    fill("grey")
+                    rect(x * this.cellSize, y * this.cellSize, this.cellSize, this.cellSize)
+                    if (this.celdas[x][y].bomba) {
+                        textSize(this.cellSize - 2)
+                        text('💣', x * this.cellSize - 1, y * this.cellSize + 16)
+                    } else {
+                        if (this.celdas[x][y].vecinos != 0) {
+                            switch (this.celdas[x][y].vecinos) {
+                                case 1:
+                                    fill("green")
+                                    break;
+                                case 2:
+                                    fill("blue");
+                                    break;
+                                case 3:
+                                    fill("red");
+                                    break;
+                                case 4:
+                                    fill("purple");
+                                    break;
+                                case 5:
+                                    fill("darkred");
+                                    break;
+                                case 6:
+                                    fill("darkgreen");
+                                    break;
+                                default:
+                                    fill("black");
+                                    break;
+                            }
+                            textSize(this.cellSize - 4)
+                            textStyle("bold")
+                            text(this.celdas[x][y].vecinos, x * this.cellSize + 6, y * this.cellSize + 17)
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
--- sketch.js
+++ sketch.js
@@ -1,18 +1,21 @@
 //Variables globales
 let tablero;
 
-
-
-
-
 function setup() {
   createCanvas(900, 600);//tamaño de la caja
-  tablero = new Grid(40,20);
+  tablero = new Grid(40, 20);
 }
 
 function draw() {
   background("white");
   tablero.display()
+}
+
+function mousePressed() {
+  let arrayX = floor(mouseX / tablero.cellSize)
+  let arrayY = floor(mouseY / tablero.cellSize)
+
+  tablero.celdas[arrayX][arrayY].descubierta = true;
 }
 
 
